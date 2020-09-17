495de91427a158c086d24982d6d0b0f6a0d15148
patience gwafan <pashgwafs@gmail.com>
2020-09-17 21:21:02 +0100

    initial commit

---
+++ ControlStatement.java
@@ -0,0 +1,18 @@
+package Work;
+public class ControlStatement {
+	public static void main(String[]args) {
+		
+	for(int counter = 0; counter <= 10;counter++){
+		
+		System.out.println("HAPPY BIRTHDAY");
+		
+	}
+	
+	int counter = 0;
+ while(counter <= 10){
+	System.out.println(counter); 
+	 counter++;
+ }
+	}
+
+}
